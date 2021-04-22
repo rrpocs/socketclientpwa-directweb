d14dc0e643bf17e7b98042946a72f29fb033efb1
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2021-04-22 11:00:10 -0300

    refactoring all

--- pages/index.js
+++ pages/index.js
@@ -67,6 +67,7 @@ export default function Home({ data }) {
       <Head>
         <title>All Peoples</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel='manifest' href='/manifest.json'></link>
       </Head>
 
       <div style={{ padding: '10px', fontSize: 'smaller' }}>
@@ -116,14 +117,30 @@ export default function Home({ data }) {
 
         <div>
           <span style={{ paddingLeft: '0px' }}>
-            mensagem:
-            <input type="text" style={{ marginLeft: '5px' }} />
+            <input type="text" style={{ marginLeft: '0px' }} />
             <input type="button" style={{ marginLeft: '5px' }}
               value="enviar" onClick={() => { sendMessage(); }} />
           </span>
         </div>
 
       </div>
+
+      <script type='javascript'>
+        // This is the "Offline page" service worker
+
+      // Add this below content to your HTML page inside a <script type="module"></script> tag, or add the js file to your page at the very top to register service worker
+      // If you get an error about not being able to import, double check that you have type="module" on your <script /> tag
+
+      /*
+        This code uses the pwa-update web component https://github.com/pwa-builder/pwa-update to register your service worker,
+        tell the user when there is an update available and let the user know when your PWA is ready to use offline.
+      */
+
+      import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
+
+      const el = document.createElement('pwa-update');
+      document.body.appendChild(el);
+      </script>
     </>
   );
 }

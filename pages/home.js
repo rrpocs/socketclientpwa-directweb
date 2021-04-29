328d733e60d380f1ac125596491c78aa4e200199
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2021-04-29 13:03:21 -0300

    refactoring

--- pages/home.js
+++ pages/home.js
@@ -12,19 +12,58 @@ export default function Home({ query }) {
   const router = useRouter()
   const { username } = router.query
   const [user, setUser] = useState({ usersList: null });
+  const [msg, setMsg] = useState("");
+  const [recMsg, setRecMsg] = useState({ listMsg: [] });
+  const [loggedUser, setLoggedUser] = useState();
+
 
   useEffect(() => {
 
     socket.emit("login", username)
-    console.log(username)
+
+    socket.on("connecteduser", data => {
+      setLoggedUser(JSON.parse(data))
+    });
 
     socket.on("users", data => {
-      console.log(data)
       setUser({ usersList: JSON.parse(data) })
+    })
+
+    socket.on("getMsg", data => {
+      let listMessages = recMsg.listMsg
+      listMessages.push(JSON.parse(data))
+      setRecMsg({ listMsg: listMessages })
     })
 
   }, [])
 
+  useEffect(() => {
+    playSound(1)
+  }, [recMsg])
+
+
+
+  const playSound = (times) => {
+    let context = new AudioContext(),
+      oscillator = context.createOscillator(),
+      contextGain = context.createGain();
+
+    oscillator.connect(contextGain);
+    contextGain.connect(context.destination);
+    oscillator.start(0);
+
+    contextGain.gain.exponentialRampToValueAtTime(
+      0.00001, context.currentTime + times
+    )
+  }
+
+  const sendMessage = () => {
+    socket.emit("sendMsg", JSON.stringify({ id: loggedUser.id, msg: msg }))
+  }
+
+  const sendNotify = () => {
+    socket.emit("sendMsg", JSON.stringify({ id: loggedUser.id, msg: 'NOTIFY' }))
+  }
 
   return (
     <div style={{ padding: '10px', fontSize: 'smaller' }}>
@@ -39,7 +78,7 @@ export default function Home({ query }) {
 
       <div style={{ padding: '0px' }}>
         usuários conectados
-        </div>
+      </div>
       <div id="divUsr" style={{ padding: '0px' }}>
         {
           user.usersList?.map(user => {
@@ -51,13 +90,38 @@ export default function Home({ query }) {
             )
           })
         }
-      </div>
+      </div><br /><br />
+
+      {/* <div>
+        mensagens trocadas
+        </div>
+      <div id="divMsg">
+        {
+          recMsg.listMsg?.map((msgInfo, index) => {
+            return (
+              <div key={index}>
+                -  {msgInfo.userName}:
+                <span style={{ paddingLeft: '2px', fontSize: 'smaller' }}>{msgInfo.msg}</span>
+                <span style={{ paddingLeft: '2px', fontSize: 'smaller' }}>{msgInfo.time}</span>
+              </div>
+            )
+          })
+        }
+      </div><br /><br /> */}
+
+
+      <div>
+        <Link href="#">
+          <a onClick={sendNotify}>NOTIFICAR</a>
+        </Link>
+      </div><br /><br />
+
 
       <div>
         <Link href="/">
           <a>VOLTAR</a>
         </Link>
-      </div>
+      </div><br /><br />
 
     </div>
   );
